f29b0102fd2b4bef15f7fae09a362fb4d3d92497
Terrence Cole <terrence.d.cole@gmail.com>
2020-01-04 08:16:06 -0800

    Use LAB colorspace.

--- shaders/draw.frag.glsl
+++ shaders/draw.frag.glsl
@@ -25,11 +25,40 @@ layout(binding = 3) uniform sampler g_sampler;
 layout(binding = 4) uniform texture2D b_texture;
 layout(binding = 5) uniform sampler b_sampler;
 
-void main() {
-    f_color = vec4(
-        texture(sampler2D(r_texture, r_sampler), v_tex_coord).r,
-        texture(sampler2D(g_texture, g_sampler), v_tex_coord).r,
-        texture(sampler2D(b_texture, b_sampler), v_tex_coord).r,
-        1.0
+vec3 lab2xyz( vec3 c ) {
+    float fy = ( c.x + 16.0 ) / 116.0;
+    float fx = c.y / 500.0 + fy;
+    float fz = fy - c.z / 200.0;
+    return vec3(
+         95.047 * (( fx > 0.206897 ) ? fx * fx * fx : ( fx - 16.0 / 116.0 ) / 7.787),
+        100.000 * (( fy > 0.206897 ) ? fy * fy * fy : ( fy - 16.0 / 116.0 ) / 7.787),
+        108.883 * (( fz > 0.206897 ) ? fz * fz * fz : ( fz - 16.0 / 116.0 ) / 7.787)
     );
+}
+
+vec3 xyz2rgb( vec3 c ) {
+	const mat3 mat = mat3(
+        3.2406, -1.5372, -0.4986,
+        -0.9689, 1.8758, 0.0415,
+        0.0557, -0.2040, 1.0570
+	);
+    vec3 v = mat * (c / 100.0);
+    vec3 r;
+    r.x = ( v.r > 0.0031308 ) ? (( 1.055 * pow( v.r, ( 1.0 / 2.4 ))) - 0.055 ) : 12.92 * v.r;
+    r.y = ( v.g > 0.0031308 ) ? (( 1.055 * pow( v.g, ( 1.0 / 2.4 ))) - 0.055 ) : 12.92 * v.g;
+    r.z = ( v.b > 0.0031308 ) ? (( 1.055 * pow( v.b, ( 1.0 / 2.4 ))) - 0.055 ) : 12.92 * v.b;
+    return r;
+}
+
+vec3 lab2rgb( vec3 c ) {
+    return xyz2rgb( lab2xyz( vec3(100.0 * c.x, 2.0 * 127.0 * (c.y - 0.5), 2.0 * 127.0 * (c.z - 0.5)) ) );
+}
+
+void main() {
+    // Project into RGB from a more linear color space to avoid causing (extra) non-uniform color shifts.
+    float l = 100 * texture(sampler2D(r_texture, r_sampler), v_tex_coord).r;
+    float a = (255 * texture(sampler2D(g_texture, g_sampler), v_tex_coord).r) - 128;
+    float b = (255 * texture(sampler2D(b_texture, b_sampler), v_tex_coord).r) - 128;
+
+    f_color = vec4(lab2rgb(vec3(l, a, b)), 1);
 }
